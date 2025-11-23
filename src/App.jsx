ac02bf6a80475cbc3d458ac0bfd266e5d6010134
tanmay-vidhate <tanmayvidhate7@gmail.com>
2025-11-23 09:31:00 +0530

    add home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from './Components/Navbar'
+import Home from './Views/Home'
 
 function App() {
   return (
     <>
       <div className='bg-[#171d32] h-auto w-full overflow-hidden'>
         <Navbar/>
+        <Home/>
       </div>
     </>
   )
--- src/Views/Home.jsx
+++ src/Views/Home.jsx
@@ -3,7 +3,14 @@ import React from 'react'
 function Home() {
     return (
         <>
-
+            <div className='text-white flex w-full justify-between items-start p-10 md:p-20'>
+                <div className='md:w-2/4 md:pt-10'>
+                    <h1 className='text-3xl md:text-6xl font-bold flex leading-normal tracking-tighter'>Hello I am Tanmay Vidhate.</h1>
+                    <p className='text-sm md:text-2xl tracking-tight'>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi ipsum vero ducimus eius quaerat, alias fuga, dolores quidem labore molestiae exercitationem facilis qui quam impedit ea eligendi. Reprehenderit, est sint!
+                    </p>
+                </div>
+            </div>
         </>
     )
 }

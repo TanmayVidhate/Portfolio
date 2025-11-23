af79a62bd9a03530f74c93b59f150b281a9d8736
tanmay-vidhate <tanmayvidhate7@gmail.com>
2025-11-23 09:30:29 +0530

    fix condition

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -27,10 +27,12 @@ function Navbar() {
                 </ul>
                 {
                     showMenu ? (
-                        <RiMenu2Line size={30} className='md:hidden absolute right-10 top-6 transition-all duration-300' onClick={() => {
-                            openMenu(!menu); setShowmenu(!showMenu)
+                        <RiMenu2Line size={30} className=' md:hidden absolute right-10 top-6 transition-all duration-300' onClick={() => {
+                            openMenu(!menu) 
                         }} />
-                    ) : <RiCloseLine size={30} className='md:hidden absolute right-10 top-6 transition-all duration-300' />
+                    ) : <RiCloseLine size={30} className='md:hidden absolute right-10 top-6 transition-all duration-300' onClick={()=>{
+                            setShowmenu(!showMenu)
+                    }} />
                 }
             </nav>
         </>

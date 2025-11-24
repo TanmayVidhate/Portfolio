1076b99700a1ec415fb7dbb5c57f8b42ecd2731f
tanmay-vidhate <tanmayvidhate7@gmail.com>
2025-11-24 14:58:56 +0530

    add flex

--- src/Views/Home.jsx
+++ src/Views/Home.jsx
@@ -4,20 +4,20 @@ import imag1 from '.././assets/hero-img.png'
 function Home() {
     return (
         <>
-            <div className='text-white flex w-full justify-between items-start p-10 md:p-20'>
-                <div className='md:w-2/4 md:pt-10'>
-                    <h1 className='text-3xl md:text-6xl font-bold flex leading-normal tracking-tighter'>
+            <div className='text-white flex w-full justify-between items-start p-10 sm:flex-col md:p-15 '>
+                <div className='border border-red-400 sm:py-5 sm:px-4 md:w-full md:pt-10'>
+                    <h1 className='  font-bold flex leading-normal tracking-tighter sm:text-4xl  md:text-5xl'>
                         Hello, I am Tanmay Vidhate.
                     </h1>
-                    <p className='text-sm md:text-2xl tracking-tight'>
+                    <p className='text-sm  tracking-tight sm:text-xl sm:mt-3  md:text-xl'>
                         I am Msc (CA) Student. MERN Stack Developer passionate about technology and web development.
                     </p>
-                    <button className='mt-5 md:md-10 text-white py02 text-sm md:text-lg md:py-2 md:px-4 hover:opacity-85 duration-300 hover:scale-105 font-semibold rounded-3xl bg-[#465697] sm:h-9 sm:w-1/2'>
+                    <button className='mt-5 text-white py02 text-sm  hover:opacity-85 duration-300 hover:scale-105 font-semibold rounded-md bg-[#465697] sm:h-10 sm:w-1/3 md:md-10 md:w-2/3 md:text-lg  md:px-4 '>
                         <a href='Tanmay_Vidhate_7028982917.pdf' download>Download Resume</a>
                     </button>
                 </div>
-                <div>
-                    <img className="" src={imag1} alt='Hero image 1'></img>
+                <div className='sm:w-full sm:block md:w-full '>
+                    <img className=" sm:m-auto sm:w-1/2" src={imag1} alt='Hero image 1'></img>
                 </div>
             </div>
         </>

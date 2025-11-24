d11d7430538b36fd5c191b19dd991d0bf15a5398
tanmay-vidhate <tanmayvidhate7@gmail.com>
2025-11-24 16:46:20 +0530

    add padding

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -7,7 +7,7 @@ function Navbar() {
     const [showMenu, setShowmenu] = useState(true);
     return (
         <>
-            <nav className='flex flex-wrap justify-between md:items-center text-white px-10 pt-6 md:px-15'>
+            <nav className='flex flex-wrap justify-between md:items-center text-white px-10 pt-6 md:px-15 lg:px-24'>
                 <span className='text-xl font-bold tracking-wide'>Portfolio</span>
                 <ul className={`${menu ? "block" : "hidden "} mx-24 p-y2 mt-4 font-semibold bg-black px-2 rounded-xl bg-opacity-30 sm:mx-60 md:mt-5 md:border-none text-center md:bg-transparent md:static md:mx-0 md:flex gap-6`}>
                     <a href='#About'>

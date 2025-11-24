31c7263369110410ea207c040c6ff24e5025b0f7
tanmay-vidhate <tanmayvidhate7@gmail.com>
2025-11-24 11:30:26 +0530

    add sm breackpoint

--- src/Views/Skills.jsx
+++ src/Views/Skills.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import { FaCss3, FaHtml5, FaJs, FaReact, FaNodeJs } from 'react-icons/fa';
-import { SiMongodb, SiExpress, SiTailwindcss, SiVite } from 'react-icons/si';
+import { FaCss3, FaHtml5, FaJs, FaReact, FaNodeJs, FaNpm  } from 'react-icons/fa';
+import { SiMongodb, SiExpress, SiTailwindcss, SiVite, SiPostman } from 'react-icons/si';
+import { VscVscode } from "react-icons/vsc";
 
 import { TbBrandGithub } from "react-icons/tb";
 
@@ -8,14 +9,14 @@ function Skills() {
   return (
     <>
       <div id="Skills" className='p-10 md:p-24'>
-        <div className='flex justify-center items-center'>
+        <div className='flex justify-center items-center  sm:flex-col sm:w-full '>
 
           {/* first div */}
           <div>
             <h1 className='text-2xl md:text-4xl text-white font-bold '>Skills</h1>
             <div className=''>
               <div className='flex flex-wrap items-center justify-start'>
-                <div className='flex flex-wrap   gap-5 md:p-12 py-10 '>
+                <div className='flex flex-wrap  w-1/2  gap-5 md:p-12 py-10  sm:w-full '>
                   <span className="p-3 bg-zinc-950 flex items-center rounded-2xl">
                     <FaHtml5 size={50} color='#E34F26' />
                   </span>
@@ -31,10 +32,6 @@ function Skills() {
                   <span className="p-3 bg-zinc-950 flex items-center rounded-2xl">
                     <FaReact size={50} color='#61DBFB' />
                   </span>
-
-                  {/* <span className="p-3 bg-zinc-950 flex items-center rounded-2xl">
-                  <SiVite size={50} color='#41D1FF' />
-                </span> */}
 
                   <span className="p-3 bg-zinc-950 flex items-center rounded-2xl">
                     <SiTailwindcss size={50} color='#06b6d4' />
@@ -61,50 +58,27 @@ function Skills() {
           </div>
 
           {/* Second Div */}
-          <div>
+          <div className=' sm:flex-col sm:w-full'>
             <h1 className='text-2xl md:text-4xl text-white font-bold'>Tools</h1>
             <div className=''>
-              <div className='flex flex-wrap items-start justify-start '>
-                <div className='flex flex-wrap  gap-5 md:p-12 py-10 '>
+              <div className='flex flex-wrap items-center justify-start  '>
+                <div className='flex flex-wrap w- gap-5 md:p-12 py-10 '>
                   <span className="p-3 bg-zinc-950 flex items-center rounded-2xl">
-                    <FaHtml5 size={50} color='#E34F26' />
+                    <SiVite size={50} color='#1572b6' />
                   </span>
 
                   <span className="p-3 bg-zinc-950 flex items-center rounded-2xl">
-                    <FaCss3 size={50} color='#1572b6' />
+                    <FaNpm size={50} color='#CC3534'  />
                   </span>
 
                   <span className="p-3 bg-zinc-950 flex items-center rounded-2xl">
-                    <FaJs size={50} color='#F0DB4F' />
+                    <SiPostman size={50} color='#EF5B25' />
                   </span>
 
                   <span className="p-3 bg-zinc-950 flex items-center rounded-2xl">
-                    <FaReact size={50} color='#61DBFB' />
+                    <VscVscode size={50} color='#007ACC' />
                   </span>
 
-                  {/* <span className="p-3 bg-zinc-950 flex items-center rounded-2xl">
-                  <SiVite size={50} color='#41D1FF' />
-                </span> */}
-
-                  <span className="p-3 bg-zinc-950 flex items-center rounded-2xl">
-                    <SiTailwindcss size={50} color='#06b6d4' />
-                  </span>
-
-                  <span className="p-3 bg-zinc-950 flex items-center rounded-2xl">
-                    <SiMongodb size={50} color='#4DB33D' />
-                  </span>
-
-                  <span className="p-3 bg-zinc-950 flex items-center rounded-2xl">
-                    <SiExpress size={50} color='#4DB33D' />
-                  </span>
-
-                  <span className="p-3 bg-zinc-950 flex items-center rounded-2xl">
-                    <FaNodeJs size={50} color='#4DB33D' />
-                  </span>
-
-                  <span className="p-3 bg-zinc-950 flex items-center rounded-2xl">
-                    <TbBrandGithub size={50} color='#fff' />
-                  </span>
                 </div>
               </div>
             </div>

a7ac32bc26a3d74625dfd55a4da9d9953980408b
tanmay-vidhate <tanmayvidhate7@gmail.com>
2025-11-24 15:21:33 +0530

    add sm breakpoint

--- src/Views/About.jsx
+++ src/Views/About.jsx
@@ -1,28 +1,28 @@
 import React from 'react'
-import Aboutimg from ".././assets/7358653-removebg-preview.png";
+import Aboutimg from ".././assets/about-us.png"
 
-import { IoArrowForward } from "react-icons/io5";
+// import { IoArrowForward } from "react-icons/io5";
 function About() {
     return (
         <>
-            <div className='text-white md:flex overflow-hidden items-center md:flex-wrap md:justify-center bg-black shadow-xl mx-0 md:mx-20 bg-opacity-30 rounded-lg p-12'>
-                <div>
-                    <h2 className='text-2xl md:text-4xl font-bold'>About</h2>
-                    <div className='md:flex flex-wrap flex-col md:flex-row items-center'>
-                        <img className="md:h-80" src={Aboutimg} alt="about image" />
+            <div id="About" className='text-white  overflow-hidden bg-black shadow-xl mx-0 items-center   md:flex md:flex-wrap md:justify-center md:mx-8 bg-opacity-30 rounded-lg p-12 '>
+                <div className='border border-red-600'>
+                    <h2 className='text-2xl font-bold md:text-4xl'>About</h2>
+                    <div className='flex-wrap flex-col items-center sm:flex sm:justify-center sm:items-center md:flex md:flex-row  border border-red-600'>
+                        <img className=" sm:w-1/2" src={Aboutimg} alt="about image" />
 
                         <ul>
-                            <div className='flex gap-3 py-4'>
+                            <div className='flex gap-3 py-1 border border-red-300'>
                                 {/* <IoArrowForward size={30} className="mt-1" /> */}
 
-                                <span className='w-96'>
-                                    <h1 className='text-xl md:text-2xl font-semibold leading-normal'>
+                                <span className='sm:w-full'>
+                                    <h1 className=' font-semibold leading-normal sm:text-3xl md:text-2xl'>
                                         MERN Stack developer
                                     </h1>
-                                    <p className='text-sm md:text-md leading-tight'>
-                                        I build full-stack applications using the MERN stack.<br/>
+                                    <p className='text-sm leading-tight sm:text-xl sm:justify-center md:text-md'>
+                                        I build full-stack applications using the MERN stack.
                                         I work on both the frontend and backend to make websites smooth and easy to use.
-                                        I enjoy learning new things in technology and improving my skills every day.<br/>
+                                        I enjoy learning new things in technology and improving my skills every day.
                                     </p>
                                 </span>
                             </div>
